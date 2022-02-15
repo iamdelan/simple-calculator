fcf9abbe2929ac72d73e8f6738d43050226b8492
iamdelan <92990983+iamdelan@users.noreply.github.com>
2022-02-15 10:01:45 +0300

    Add files via upload

---
+++ simple calculator/simple calculator/simple calculator/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1, num2;
+    int sum, sub, mult, mod;
+    float div;
+
+    /*
+     *input two numbers from user
+     */
+     printf("Enter any two numbers: ");
+     scanf("%d%d", &num1, &num2);
+
+     /*
+      *perform all arithmetic operations
+      */
+      sum = num1 + num2;
+      sub = num1 - num2;
+      mult = num1 *num2;
+      div =(float)num1 / num2;
+      mod =num1 % num2;
+
+      /*
+       * Print result of all arithmetic operations
+       */
+    printf("SUM = %d\n", sum);
+    printf("DIFFERENCE = %d\n", sub);
+    printf("PRODUCT = %d\n", mult);
+    printf("QUOTIENT = %f\n", div);
+    printf("MODULUS = %d", mod);
+
+      return 0;
+}
